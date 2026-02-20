7721dc64b6d6d1966ed6ea911321a7bdfc1fb4cf
L3x4-4 <recai123>
2026-02-20 19:05:14 +0300

    sda

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace noteBookLM_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            //Console.Write("Lütfen bir sayı yazınız: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Girdiğiniz sayının karesi: ");
+            //Console.WriteLine(KareAl(sayi));
+            //Console.Write("Girdiğiniz sayının küpü: ");
+            //Console.WriteLine(KupAl(sayi));
+
+
+        }
+
+        //static int KareAl(int number)
+        //{
+
+        //    return number * number;
+        //}
+        //static int KupAl(int number)
+        //{
+        //    return number*number*number;
+        //}
+
+        //static string EkranaYaz(string mesaj , int tekrarSayisi)
+        //{
+        //    for (int i= 0; i<tekrarSayisi;i++)
+        //    {
+        //        Console.WriteLine((i+1)+". "+mesaj);
+        //    }
+
+        //    return mesaj;
+
+        //}
+
+
+
+
+    }
+}
